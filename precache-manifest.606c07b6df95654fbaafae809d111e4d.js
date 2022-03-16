81619e21202c00d574389090135c914b2362c3f1
Yashwant <yashwant317201@gmail.com>
2022-03-16 17:16:55 +0530

    Updates

---
+++ precache-manifest.606c07b6df95654fbaafae809d111e4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aac7e069f56190a24c736d1b3117274",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/Todo-List/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "7ccda747e34119fe0df8",
+    "url": "/Todo-List/static/js/main.8f0d7291.chunk.js"
+  },
+  {
+    "revision": "b202ee01070b94d1df7a",
+    "url": "/Todo-List/static/js/runtime-main.d0f845fd.js"
+  }
+]);
